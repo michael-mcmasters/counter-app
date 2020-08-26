510d110fc0c270d2c62a0fa156b08cdd063ca788
PalmTreeBlvd <55242169+PalmTreeBlvd@users.noreply.github.com>
2020-08-26 17:22:31 -0400

    first commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,11 @@
+// Import React and React Component (snippet is imrc)
+import React, { Component } from "react";
+
+// (Snippet to create class is cc)
+class Counter extends Component {
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+export default Counter;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// React is an object imported from the 'react' module. It is needed to compile jsx into Javascript.
+import React from "react";
+// Import this to send jsx code that was converted to virtual DOM to the real DOM
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import "bootstrap/dist/css/bootstrap.css";
+import Counter from "./components/counter";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Counter />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
