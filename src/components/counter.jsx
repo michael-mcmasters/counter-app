4c66d3438498b3832d5f3af7adca36dd802fd74d
PalmTreeBlvd <55242169+PalmTreeBlvd@users.noreply.github.com>
2020-08-27 03:08:19 -0400

    commit

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,8 +3,41 @@ import React, { Component } from "react";
 
 // (Snippet to create class is cc)
 class Counter extends Component {
+  // property
+  state = {
+    count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+  };
+
   render() {
-    return <h1>Hello World</h1>;
+    return (
+      <div>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          // "for-loop" to render each item in list. Check notes. Or watch
+          @52:01.
+          {this.state.tags.map((tag) => (
+            <li key={tag}>{tag}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+
+  // There are 3 classes, badge, m-2, and (badge-warning or badge-primary).
+  // warning makes background orange. primary makes background blue. Function determines and returns which to use.
+  getBadgeClasses() {
+    let classes = "badge m-2 badge-";
+
+    // 3rd class is badge-something. Determine if it should be 'warning' or 'primary' and add it to string.
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,12 +5,10 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+// Bootstrap: A CSS library that gives our application a modern look and feel.
 import "bootstrap/dist/css/bootstrap.css";
 import Counter from "./components/counter";
 
 ReactDOM.render(<Counter />, document.getElementById("root"));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
